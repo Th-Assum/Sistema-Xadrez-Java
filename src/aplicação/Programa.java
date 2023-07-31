4b01c889f76fe9dd5939fac39375f469848a5df6
Thiago Assum <Thiagoassum97@gmail.com>
2023-07-31 20:50:21 -0300

    Primeira classe: Posição

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -1,8 +1,14 @@
 package aplicação;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+		
 	
 	}
 
